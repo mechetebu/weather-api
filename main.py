fa3e729ceda3ac6b128606c2123c9b5a31b92ec4
Mike <you@example.com>
2025-09-02 23:55:30 -0400

    Updated kwargs handling in ingest function and added key parameter to load_json_to_s3 function

--- main.py
+++ main.py
@@ -26,29 +26,31 @@ s3 = boto3.client('s3')
 def get_current_weather(URI: str) -> json:
     r = requests.get(URI)
     if  r.status_code != 200:
-        return logging.exception(f"Code failed due to API status: {status}. Please retry")
+        return logging.exception(f"Code failed due to API status: {r.status_code}. Please retry")
     else: 
         data = r.json()
         logging.info(data)
         return data
-def load_json_to_s3(data: json, bucket = bucket_name) -> None:
+def load_json_to_s3(data: json, key, bucket = bucket_name) -> None:
         try:
             logging.info(f'Current date_time: {datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}')
-            filename = f'Raw_api_data/{CITY}_Weather_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json'
+            #filename = f'Raw_api_data/{CITY}_Weather_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json'
             s3.put_object(
             Body = json.dumps(data),
             Bucket = bucket,
-            Key = filename
+            Key = key
             )
         except Exception as e :
-            logging.exception(f"Failed to upload: {filename}", exc_info = True)
+            logging.exception(f"Failed to upload: {key}", exc_info = True)
         finally:
-            logging.info(f'The file: {filename} was uploaded at {datetime.now()}.')
+            logging.info(f'The file: {key} was uploaded at {datetime.now()}.')
 def ingest(get_current_weather, load_json_to_s3, *args, **kwargs):
-    data = get_current_weather(*args, **kwargs)
-    load_json_to_s3(data)
+    get_current_weather_kwargs = {'URI': kwargs.pop("URI")}
+    load_json_to_s3_kwargs = {'key': kwargs.pop('key')}
+    data = get_current_weather(*args, **get_current_weather_kwargs)
+    load_json_to_s3(data, **load_json_to_s3_kwargs)
     
-def flatten_raw_api_data(data)-> dict:
+def flatten_raw_api_data(data: dict)-> json:
         record = {}
         for key, value in data.items():
             if isinstance(value, dict):
@@ -60,13 +62,16 @@ def flatten_raw_api_data(data)-> dict:
                 record[key] = value
         return record
 if __name__ == "__main__":
-    ingest(get_current_weather, load_json_to_s3, URI = ZIP_CODE_CURRENT_API)
+    ingest(get_current_weather, load_json_to_s3, URI = ZIP_CODE_CURRENT_API, key = f'Raw_api_data/{CITY}_Weather_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json')
         
         
 
 #Processing step
-def retrieve_json_from_S3(key, bucket_name):
-    pass
+def retrieve_json_from_S3(key, bucket_name)-> dict:
+    response = s3.get_object(Bucket=bucket_name, Key= key)
+    json_content = (response['Body'].read().decode('utf-8'))
+    data  = json.loads(json_content)
+    return data
 
 '''
 TODO
